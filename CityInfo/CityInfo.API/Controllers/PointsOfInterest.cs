67d218f6ded2d475dbe0dc8b74177012f56533c9
Your Name <you@example.com>
2021-11-17 17:02:48 +0000

    Creating a Resource

--- CityInfo/CityInfo.API/Controllers/PointsOfInterest.cs
+++ CityInfo/CityInfo.API/Controllers/PointsOfInterest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CityInfo.API.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,23 +9,57 @@ namespace CityInfo.API.Controllers
 {
     [ApiController]
     [Route("api/cities/{cityId}/pointsofinterest")]
-    public class PointsOfInterest: ControllerBase
+    public class PointsOfInterest : ControllerBase
     {
         [HttpGet("{id}")]
         public IActionResult GetPointsOfInterest(int cityId, int id)
         {
             var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
-            if( city == null)
+            if (city == null)
             {
                 return NotFound();
             }
             // find point of interest
             var pointOfInterest = city.PointsOfInterest.FirstOrDefault(c => c.Id == id);
-            if( pointOfInterest == null)
+            if (pointOfInterest == null)
             {
                 return NotFound();
             }
             return Ok(pointOfInterest);
+        }
+
+        [HttpPost("{id}", Name="GetPointOfInterest")]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody]PointOfinterestForCreationDto pointOfInterest)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if(city == null)
+            {
+                return NotFound();
+            }
+
+            var maxPointOfInterestId = CitiesDataStore.Current.Cities.SelectMany(
+                c => c.PointsOfInterest).Max(p => p.Id);
+
+            var finalPointOfInterest = new PointOfInterestDto()
+            {
+                Id = ++maxPointOfInterestId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+            city.PointsOfInterest.Add(finalPointOfInterest);
+
+            //CreatedAtRoute can receive 3 parameters:
+
+            //routeName Is the name that you must put on the method that will be the URI that would get that resource after created.
+
+            //routeValues It's the object containing the values that will be passed to the GET method at the named route. It will be used to return the created object
+
+            //content It's the object that was created.
+            return CreatedAtRoute(
+                "GetPointOfInterest",
+                new { cityId, id = finalPointOfInterest.Id },
+                finalPointOfInterest);
+
         }
     }
 }
