0bcbb50a49d9cbb24de28a13b232344dc7d36391
Your Name <you@example.com>
2021-11-18 16:59:47 +0000

    Creating a DbContext

---
+++ CityInfo/CityInfo.API/Contexts/CityInfoContext.cs
@@ -0,0 +1,32 @@
+﻿using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Contexts
+{
+    public class CityInfoContext : DbContext
+    {
+        //Dbset can be used to query and save instances of its entity type
+        //link queries against the Dbset will be translated into queries against the database
+        public DbSet<City> Cities { get; set; }
+        public DbSet<PointOfInterest> PointOfInterests { get; set; }
+
+        //this is the constructor that allow the config to use the onnection string
+        public CityInfoContext(DbContextOptions<CityInfoContext> options):base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+
+        ////another way of having connection string 
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //tell the dbcontext to connect to sql server
+        //    optionsBuilder.UseSqlServer("connectionstring");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+    }
+}
--- CityInfo/CityInfo.API/Startup.cs
+++ CityInfo/CityInfo.API/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CityInfo.API.Contexts;
 using CityInfo.API.Servieces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -34,6 +36,11 @@ namespace CityInfo.API
             //if running release build, use clouldmailservice
             services.AddTransient<IMailService, CloudMailService>();
 #endif
+            var connectionString = @"Server=(localdb)\mssqllocaldb;Database=CityInfoDB;Trusted_Connection=True;";
+            services.AddDbContext<CityInfoContext>(o =>
+            {
+                o.UseSqlServer(connectionString);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
