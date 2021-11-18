ad59c0a2c5b7df6d41f4b18ed4612506aacd9f0c
Your Name <you@example.com>
2021-11-18 16:11:18 +0000

    Creating Entity Classes

---
+++ CityInfo/CityInfo.API/Entities/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        public ICollection<PointOfInterest> PointOfInterests { get; set; } = new List<PointOfInterest>();
+    }
+}
---
+++ CityInfo/CityInfo.API/Entities/PointOfInterest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "You should provide a name value.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        //navigation properties( navigate the point of interest to the parent city
+        public City City { get; set; }
+        public int CityId { get; set; }
+
+        //A primary key constrain is a column that uniquely identifies every row in the table of the relational database management system, 
+        //while foreign key is a column that creates a relationship between two tables.
+    }
+}
